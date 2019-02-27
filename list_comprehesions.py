6646ea1e0e9adb157872ba86b68d26060cb9f35e
purlubru <polli2498@gmail.com>
2019-02-27 23:34:12 +0300

    Add files via upload

---
+++ list_comprehesions.py
@@ -0,0 +1,13 @@
+#a=input().split(" ")
+#print(a)
+#b=[int(el)+3 for el in a]
+#print(b)
+
+#a=input().split(" ")
+#b=[el+ '*' for el in a]
+#print(b)
+
+#from math import sqrt
+#a=input().split(" ")
+#b=[sqrt(int(el)) for el in a[::2]]
+#print(b)
