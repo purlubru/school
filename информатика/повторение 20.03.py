d46c9b4c4ac2043448b871dcea7fedba2e965aad
purlubru <polli2498@gmail.com>
2019-03-20 14:44:56 +0300

    Add files via upload

---
+++ информатика/повторение 20.03.py
@@ -0,0 +1,63 @@
+#n = int(input())
+#m = int(input())
+#x = int(input())
+#y = int(input())
+#if n > m:
+ #   a = n
+  #  b = m
+##else:
+##    a = m
+##    b = n
+##if a - x > x:
+##    if b - y > y:
+##        if x > y:
+##            print(y)
+##        else:
+##            print(x)
+##    else:
+##        if x > b - y:
+##            print(b - y)
+##        else:
+##            print(x)
+##else:
+##    if b - y > y:
+##        if a - x > y:
+##            print(y)
+##        else:
+##            print(a - x)
+##    else:
+##        if a - x > b - y:
+##            print(b - y)
+##        else:
+##            print(a - x)
+            
+##m = int(input())
+##n = int(input())
+##k = int(input())
+##if k%m == 0 or k%n == 0 :
+##    print("yes")
+##else:
+##    print('no')
+
+
+
+
+##v = int(input())
+##t = int(input())
+#s = v*t
+#print(s%109)
+
+n=int(input())
+for i in range(n+1):
+    for g in range(i):
+        print(g+1, end='')
+    #print('\n', end='')
+
+
+
+
+
+
+
+
+
